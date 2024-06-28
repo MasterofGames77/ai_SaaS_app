054110c44a5edca76d0c72c7ad9e4638ee70f3b8
MasterofGames77 <mgambardella16@gmail.com>
2024-06-28 12:22:57 -0400

    implement sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,14 +34,6 @@ export default function RootLayout({
     >
       <html lang="en">
         <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
-          {/* <header>
-            <SignedOut>
-              <SignInButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header> */}
           {children}
         </body>
       </html>
